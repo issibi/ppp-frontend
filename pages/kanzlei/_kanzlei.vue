1b70c7ae19437796dc68a6984d87cc9f022a83dd
Jan Boldis <jboldis.forwarding@gmail.com>
2023-05-06 12:04:00 +0200

    ready

--- pages/kanzlei/_kanzlei.vue
+++ pages/kanzlei/_kanzlei.vue
@@ -1,24 +1,93 @@
 <template>
   <article :class="'show-' + show">
-    <div v-if="show">
-      <img
-        v-if="getPage.attributes.Image_mobile.data !== null"
-        class="background-mobile"
-        :src="
-          'https://api.ppp.co.at/' +
-          getPage.attributes.Image_mobile.data.attributes.url
-        "
-        alt=""
-      />
-    </div>
     <main v-if="show">
-      <h1 v-html="formatTitle(getPage.attributes.Title)"></h1>
-      <div v-html="formatRte(getPage.attributes.Content)"></div>
-      <div v-if="false" v-html="formatRte(getPage.attributes.Examples)"></div>
-      <div
-        v-if="false"
-        v-html="formatRte(getPage.attributes.Publications)"
-      ></div>
+      <Transition>
+        <a
+          v-if="showEx || showPub"
+          class="close"
+          @click="(showEx = false), (showPub = false)"
+        >
+          <svg
+            width="27"
+            height="27"
+            viewBox="0 0 27 27"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <mask
+              id="mask0_0_1"
+              style="mask-type: luminance"
+              maskUnits="userSpaceOnUse"
+              x="2"
+              y="2"
+              width="24"
+              height="24"
+            >
+              <path
+                d="M25.1437 2.14453H2.1543V25.1339H25.1437V2.14453Z"
+                fill="white"
+              />
+            </mask>
+            <g mask="url(#mask0_0_1)">
+              <path
+                d="M25.1437 2.14453H2.1543V25.1339H25.1437V2.14453Z"
+                fill="#B1AA8D"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M0.883832 28.9307L28.9362 0.878295L26.4095 -1.64844L-1.6429 26.4039L0.883832 28.9307Z"
+                fill="white"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M28.9392 26.4055L0.886779 -1.64685L-1.63995 0.879883L26.4124 28.9323L28.9392 26.4055Z"
+                fill="white"
+              />
+            </g>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M0.420959 0.412598H26.877V26.8686H0.420959V0.412598ZM3.88763 3.87927V23.402H23.4103V3.87927H3.88763Z"
+              fill="#B1AA8D"
+            />
+          </svg>
+        </a>
+      </Transition>
+      <Transition>
+        <div v-if="!showEx && !showPub">
+          <h1 v-html="formatTitle(getPage.attributes.Title)"></h1>
+          <div v-html="formatRte(getPage.attributes.Content)"></div>
+        </div>
+      </Transition>
+      <Transition>
+        <div class="detail" v-if="showEx">
+          <div v-html="formatRte(getPage.attributes.Examples)"></div>
+        </div>
+      </Transition>
+      <Transition>
+        <div class="detail" v-if="showPub">
+          <div v-html="formatRte(getPage.attributes.Publications)"></div>
+        </div>
+      </Transition>
+      <Transition>
+        <div class="menu" v-if="!showEx && !showPub">
+          <a
+            class="switch"
+            v-if="getPage.attributes.Examples"
+            @click="showEx = !showEx"
+            >BEISPIELE ></a
+          >
+          <br />
+          <a
+            class="switch"
+            v-if="getPage.attributes.Publications"
+            @click="showPub = !showPub"
+            >PUBLIKATIONEN ></a
+          >
+        </div>
+      </Transition>
     </main>
 
     <div v-if="show">
@@ -32,8 +101,7 @@
         alt=""
       />
     </div>
-
-    <aside v-if="show">
+    <aside v-if="show && !showEx && !showPub">
       <div v-html="formatRte(getPage.attributes.Hashtags)"></div>
     </aside>
   </article>
@@ -50,11 +118,10 @@ export default {
   name: "kanzlei",
   data() {
     return {
+      showEx: false,
+      showPub: false,
       show: false,
-      data: {
-        pages: null,
-        profiles: null,
-      },
+      pages: null,
     };
   },
   methods: {
@@ -62,7 +129,7 @@ export default {
       const getPages = await axios.get(
         "https://api.ppp.co.at//api/pages?populate=*"
       );
-      this.data.pages = getPages.data.data;
+      this.pages = getPages.data.data;
     },
     formatRte(str) {
       if (str !== null && str !== undefined) {
@@ -87,8 +154,8 @@ export default {
   },
   computed: {
     getPage() {
-      if (this.data.pages == null) return false;
-      let filtered_pages = this.data.pages;
+      if (this.pages == null) return false;
+      let filtered_pages = this.pages;
       let page = filtered_pages.filter((e) => {
         return e.attributes.Slug === this.$route.params.kanzlei;
       });
--- pages/team/_team.vue
+++ pages/team/_team.vue
@@ -1,60 +1,60 @@
 <template>
   <article :class="'show-' + show">
-    <main v-if="show">
-      <Transition>
-        <a
-          v-if="showCV || showPub"
-          class="close"
-          @click="(showCV = false), (showPub = false)"
+    <Transition>
+      <a
+        v-if="showCV || showPub"
+        class="close"
+        @click="(showCV = false), (showPub = false)"
+      >
+        <svg
+          width="27"
+          height="27"
+          viewBox="0 0 27 27"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <svg
-            width="27"
-            height="27"
-            viewBox="0 0 27 27"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+          <mask
+            id="mask0_0_1"
+            style="mask-type: luminance"
+            maskUnits="userSpaceOnUse"
+            x="2"
+            y="2"
+            width="24"
+            height="24"
           >
-            <mask
-              id="mask0_0_1"
-              style="mask-type: luminance"
-              maskUnits="userSpaceOnUse"
-              x="2"
-              y="2"
-              width="24"
-              height="24"
-            >
-              <path
-                d="M25.1437 2.14453H2.1543V25.1339H25.1437V2.14453Z"
-                fill="white"
-              />
-            </mask>
-            <g mask="url(#mask0_0_1)">
-              <path
-                d="M25.1437 2.14453H2.1543V25.1339H25.1437V2.14453Z"
-                fill="#B1AA8D"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M0.883832 28.9307L28.9362 0.878295L26.4095 -1.64844L-1.6429 26.4039L0.883832 28.9307Z"
-                fill="white"
-              />
-              <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
-                d="M28.9392 26.4055L0.886779 -1.64685L-1.63995 0.879883L26.4124 28.9323L28.9392 26.4055Z"
-                fill="white"
-              />
-            </g>
+            <path
+              d="M25.1437 2.14453H2.1543V25.1339H25.1437V2.14453Z"
+              fill="white"
+            />
+          </mask>
+          <g mask="url(#mask0_0_1)">
+            <path
+              d="M25.1437 2.14453H2.1543V25.1339H25.1437V2.14453Z"
+              fill="#B1AA8D"
+            />
             <path
               fill-rule="evenodd"
               clip-rule="evenodd"
-              d="M0.420959 0.412598H26.877V26.8686H0.420959V0.412598ZM3.88763 3.87927V23.402H23.4103V3.87927H3.88763Z"
-              fill="#B1AA8D"
+              d="M0.883832 28.9307L28.9362 0.878295L26.4095 -1.64844L-1.6429 26.4039L0.883832 28.9307Z"
+              fill="white"
             />
-          </svg>
-        </a>
-      </Transition>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M28.9392 26.4055L0.886779 -1.64685L-1.63995 0.879883L26.4124 28.9323L28.9392 26.4055Z"
+              fill="white"
+            />
+          </g>
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M0.420959 0.412598H26.877V26.8686H0.420959V0.412598ZM3.88763 3.87927V23.402H23.4103V3.87927H3.88763Z"
+            fill="#B1AA8D"
+          />
+        </svg>
+      </a>
+    </Transition>
+    <main v-if="show">
       <Transition>
         <div v-if="!showCV && !showPub && showProfile">
           <div v-html="formatRte(showProfile.attributes.Basic_info)"></div>
@@ -133,9 +133,7 @@ export default {
       show: false,
       showCV: false,
       showPub: false,
-      data: {
-        profiles: null,
-      },
+      profiles: null,
     };
   },
   methods: {
@@ -143,7 +141,7 @@ export default {
       const getProfiles = await axios.get(
         "https://api.ppp.co.at//api/profiles?populate=*"
       );
-      this.data.profiles = getProfiles.data.data;
+      this.profiles = getProfiles.data.data;
     },
     formatRte(str) {
       if (str !== null && str !== undefined) {
@@ -168,8 +166,8 @@ export default {
   },
   computed: {
     showProfile() {
-      if (this.data.profiles == null) return false;
-      let filtered_profiles = this.data.profiles;
+      if (this.profiles == null) return false;
+      let filtered_profiles = this.profiles;
       let profile = filtered_profiles.filter((e) => {
         return e.attributes.Slug === this.$route.params.team;
       });
